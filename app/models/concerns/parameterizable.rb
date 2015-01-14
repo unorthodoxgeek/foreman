d2c01441e371001b5c7fe02d1fd0cdf288686f1b
Tom Caspy <tcaspy@gmail.com>
2015-01-14 14:12:23 +0200

    fixing eLobato's comments

--- app/models/concerns/parameterizable.rb
+++ app/models/concerns/parameterizable.rb
@@ -3,25 +3,29 @@ module Parameterizable
     string.gsub(/[\/.>]/, '-').gsub(/[<!*'();:@&=+$,?%#\[\]]/, '').chomp('-')
   end
 
+  module Finder
+    def find(*args)
+      if args.size == 1 && (args.first.is_a?(String) || args.first.is_a?(Numeric))
+        from_param(args.first)
+      else
+        super
+      end
+    end
+  end
+
   module ById
     extend ActiveSupport::Concern
 
     included do
+      extend ::Parameterizable::Finder
       def to_param
         id.to_s
       end
 
       def self.from_param(id)
-        self.where(id: id.to_i).first
+        self.where(:id => id.to_i).first
       end
 
-      def self.find(*args)
-        if args.size == 1 && [String, Integer].include?(args.first.class)
-          from_param(args.first)
-        else
-          super
-        end
-      end
     end
   end
 
@@ -29,21 +33,14 @@ module Parameterizable
     extend ActiveSupport::Concern
 
     included do
+      extend ::Parameterizable::Finder
       def to_param
         # remove characters unsafe for urls, keep unicode ones
         Parameterizable.parameterize("#{id}-#{name}")
       end
 
       def self.from_param(id_name)
-        self.where(id: id_name.to_i).first
-      end
-
-      def self.find(*args)
-        if args.size == 1 && (args.first.is_a?(String) || args.first.is_a?(Numeric))
-          from_param(args.first)
-        else
-          super
-        end
+        self.where(:id => id_name.to_i).first
       end
     end
   end
@@ -56,21 +53,19 @@ module Parameterizable
     # Check the name format validator before use.
 
     included do
+      extend ::Parameterizable::Finder
       def to_param
         name
       end
 
       def self.from_param(name)
-        self.where(name: name).first
-      end
-
-      def self.xfind(*args)
-        if args.size == 1 && [String, Integer].include?(args.first.class)
-          from_param(args.first)
+        if name.is_a?(Numeric)
+          self.where(:id => name).first
         else
-          super
+          self.where(:name => name).first
         end
       end
+
     end
   end
 end
